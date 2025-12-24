cfd0c6d044e65ffcaedf437d3efe6cd44ebe745d
wighawag <wighawag@gmail.com>
2025-12-24 09:13:16 +0000

    we pass in the provider to the handler so handler get the actual provider being used + we ensure viem use the extended provider

--- src/index.ts
+++ src/index.ts
@@ -35,14 +35,115 @@ export interface ProviderOptions {
 		  }
 	);
 	doNotFillMissingFields?: boolean;
-	handlers?: Record<string, (params?: any[]) => Promise<any>>;
+	handlers?: Record<string, (provider: EIP1193ProviderWithoutEvents, params?: any[]) => Promise<any>>;
 }
 
 export function extendProviderWithAccounts(
-	provider: EIP1193ProviderWithoutEvents,
+	providerToExtend: EIP1193ProviderWithoutEvents,
 	options?: ProviderOptions,
 ): EIP1193ProviderWithoutEvents {
 	let clients: {wallet: WalletClient<Transport, Chain>; public: PublicClient<Transport, Chain>} | undefined;
+
+	const accountHandlers: Record<string, (provider: EIP1193ProviderWithoutEvents, params: any[]) => Promise<any>> = {
+		eth_sendTransaction: async (provider, params) => {
+			const tx: EIP1193TransactionData = params[0];
+			if (options?.doNotFillMissingFields) {
+				validateTransaction(tx);
+			}
+			const viemTx = toViemTransaction(tx);
+			const account = accounts.find((a) => a.address.toLowerCase() === tx.from.toLowerCase());
+			const impersonate = options?.impersonate;
+			const clients = await getClients();
+			if (impersonate?.mode !== 'always' && account) {
+				return clients.wallet.sendTransaction({
+					...viemTx,
+					account,
+				} as any);
+			} else if (impersonate) {
+				if (shouldImpersonate(tx.from)) {
+					await impersonate.impersonator.impersonateAccount({
+						address: tx.from as `0x${string}`,
+					});
+
+					return await provider.request({
+						method: 'eth_sendTransaction',
+						params: [tx],
+					});
+				} else {
+					throw new Error('Account not available, not even as impersonation');
+				}
+			} else {
+				throw new Error('Account not available');
+			}
+		},
+		eth_accounts: async () => {
+			return accounts.map((a) => a.address);
+		},
+		eth_requestAccounts: async () => {
+			return accounts.map((a) => a.address);
+		},
+		personal_sign: async (provider, params) => {
+			const [message, address] = params;
+			const account = accounts.find((a) => a.address === address);
+			if (!account) {
+				throw new Error('Account not available for signing');
+			}
+			const prefixedMessage = `\x19Ethereum Signed Message:\n${message.length}${message}`;
+			return account.signMessage({message: prefixedMessage});
+		},
+		eth_sign: async (provider, params) => {
+			const [address, message] = params;
+			const account = accounts.find((a) => a.address === address);
+			if (!account) {
+				throw new Error('Account not available for signing');
+			}
+			return account.signMessage({message});
+		},
+		eth_signTransaction: async (provider, params) => {
+			throw new Error('eth_signTransaction not implemented');
+			// const tx = params[0];
+			// const account = accounts.find((a) => a.address === tx.from);
+			// if (!account) {
+			// 	throw new Error('Account not available for signing');
+			// }
+			// return account.signTransaction(signTxParams);
+		},
+		eth_signTypedData: async (provider, params) => {
+			const [address, typedData] = params;
+			const account = accounts.find((a) => a.address === address);
+			if (!account) {
+				throw new Error('Account not available for signing');
+			}
+			return account.signTypedData(typedData);
+		},
+		eth_signTypedData_v4: async (provider, params) => {
+			const [address, typedData] = params;
+			const account = accounts.find((a) => a.address === address);
+			if (!account) {
+				throw new Error('Account not available for signing');
+			}
+			return account.signTypedData(typedData);
+		},
+	};
+
+	const handlers: Record<string, (provider: EIP1193ProviderWithoutEvents, params: any[]) => Promise<any>> = {
+		...accountHandlers,
+		...options?.handlers,
+	};
+	const provider = {
+		request: async (args: {method: string; params?: any[]}) => {
+			const {method, params = []} = args;
+			const handler = handlers[method];
+			if (!handler) {
+				return provider.request({
+					method: args.method,
+					params: args.params,
+				} as any);
+			}
+			return handler(provider, params);
+		},
+	} as EIP1193ProviderWithoutEvents;
+
 	async function getClients() {
 		if (clients) {
 			return clients;
@@ -171,103 +272,5 @@ export function extendProviderWithAccounts(
 		}
 	}
 
-	const accountHandlers: Record<string, (params: any[]) => Promise<any>> = {
-		eth_sendTransaction: async (params) => {
-			const tx: EIP1193TransactionData = params[0];
-			if (options?.doNotFillMissingFields) {
-				validateTransaction(tx);
-			}
-			const viemTx = toViemTransaction(tx);
-			const account = accounts.find((a) => a.address.toLowerCase() === tx.from.toLowerCase());
-			const impersonate = options?.impersonate;
-			const clients = await getClients();
-			if (impersonate?.mode !== 'always' && account) {
-				return clients.wallet.sendTransaction({
-					...viemTx,
-					account,
-				} as any);
-			} else if (impersonate) {
-				if (shouldImpersonate(tx.from)) {
-					await impersonate.impersonator.impersonateAccount({
-						address: tx.from as `0x${string}`,
-					});
-
-					return await provider.request({
-						method: 'eth_sendTransaction',
-						params: [tx],
-					});
-				} else {
-					throw new Error('Account not available, not even as impersonation');
-				}
-			} else {
-				throw new Error('Account not available');
-			}
-		},
-		eth_accounts: async () => {
-			return accounts.map((a) => a.address);
-		},
-		eth_requestAccounts: async () => {
-			return accounts.map((a) => a.address);
-		},
-		personal_sign: async (params) => {
-			const [message, address] = params;
-			const account = accounts.find((a) => a.address === address);
-			if (!account) {
-				throw new Error('Account not available for signing');
-			}
-			const prefixedMessage = `\x19Ethereum Signed Message:\n${message.length}${message}`;
-			return account.signMessage({message: prefixedMessage});
-		},
-		eth_sign: async (params) => {
-			const [address, message] = params;
-			const account = accounts.find((a) => a.address === address);
-			if (!account) {
-				throw new Error('Account not available for signing');
-			}
-			return account.signMessage({message});
-		},
-		eth_signTransaction: async (params) => {
-			throw new Error('eth_signTransaction not implemented');
-			// const tx = params[0];
-			// const account = accounts.find((a) => a.address === tx.from);
-			// if (!account) {
-			// 	throw new Error('Account not available for signing');
-			// }
-			// return account.signTransaction(signTxParams);
-		},
-		eth_signTypedData: async (params) => {
-			const [address, typedData] = params;
-			const account = accounts.find((a) => a.address === address);
-			if (!account) {
-				throw new Error('Account not available for signing');
-			}
-			return account.signTypedData(typedData);
-		},
-		eth_signTypedData_v4: async (params) => {
-			const [address, typedData] = params;
-			const account = accounts.find((a) => a.address === address);
-			if (!account) {
-				throw new Error('Account not available for signing');
-			}
-			return account.signTypedData(typedData);
-		},
-	};
-
-	const handlers: Record<string, (params: any[]) => Promise<any>> = {
-		...accountHandlers,
-		...options?.handlers,
-	};
-	return {
-		request: async (args: {method: string; params?: any[]}) => {
-			const {method, params = []} = args;
-			const handler = handlers[method];
-			if (!handler) {
-				return provider.request({
-					method: args.method,
-					params: args.params,
-				} as any);
-			}
-			return handler(params);
-		},
-	} as EIP1193ProviderWithoutEvents;
+	return provider;
 }
